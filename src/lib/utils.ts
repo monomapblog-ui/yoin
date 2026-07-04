cfe9c507710ec5d50844d39a4217484e67bc97c3
rakuhai <monomapblog@gmail.com>
2026-07-04 16:31:51 +0900

    add missing utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,6 @@
+﻿import { format } from "date-fns";
+import { ja } from "date-fns/locale";
+
+export function formatDate(date: Date | string): string {
+  return format(new Date(date), "yyyy年M月d日", { locale: ja });
+}
